0150038c58d1f40a7429107f2c6aa6c0f5a629e8
MithunKrishna26 <mithun2605krishna@gmail.com>
2025-11-27 17:02:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "RoutePulse service online"
+def home():
+    return "Hello, Jenkins Multi-Stage Pipeline!"
 
 if __name__ == "__main__":
-    # Bind to 0.0.0.0 so container is reachable from outside
-    app.run(host="0.0.0.0", port=12144)
+    app.run(debug=True, host='0.0.0.0', port=12144)
